0385f8c642ee185e1c33bac81f7b03e09b2513f2
Luciano Muñoz <luchisds@gmail.com>
2022-07-02 23:48:47 -0300

    🗃️  Create a custom User model

--- cobranzas/app/models.py
+++ cobranzas/app/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_collector = models.BooleanField(default=False, verbose_name=_('Is a collector?'))
--- cobranzas/cobranzas/settings.py
+++ cobranzas/cobranzas/settings.py
@@ -134,3 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'app.User'
