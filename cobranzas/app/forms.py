cb5eb36e240092864a2409f616857cf4dc55f754
Luciano Muñoz <luchisds@gmail.com>
2022-07-04 23:19:28 -0300

    ➕ Add crispy-forms package

--- cobranzas/app/forms.py
+++ cobranzas/app/forms.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from app.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'create-user-form'
+        self.helper.form_class = 'create-form'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('signup')
+        self.helper.add_input(Submit('submit', 'Crear Usuario'))
 
     class Meta(UserCreationForm.Meta):
         model = User
