c34fe77e7c106ddca5c383780f046d2175ab1cbf
Luciano Muñoz <luchisds@gmail.com>
2022-07-17 17:29:55 -0300

    ✨ Update forms and sale creation view logic to support product selection

--- cobranzas/app/forms.py
+++ cobranzas/app/forms.py
@@ -32,10 +32,18 @@ class CustomerCreationForm(forms.ModelForm):
 
 
 class SaleCreationForm(forms.ModelForm):
+    price = forms.FloatField(disabled=True)
 
     class Meta:
         model = Sale
         fields = '__all__'
 
 
-SaleProductFormSet = inlineformset_factory(Sale, SaleProduct, form=SaleCreationForm, fields='__all__', extra=1, can_delete=False, absolute_max=50, max_num=50)
+class SaleProductCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = SaleProduct
+        fields = '__all__'
+
+
+SaleProductFormSet = inlineformset_factory(Sale, SaleProduct, form=SaleProductCreationForm, fields='__all__', extra=1, can_delete=False, absolute_max=50, max_num=50)
