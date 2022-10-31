9ee11e7e7ce5db3c2281596b04b0b9d0ba8409f4
Luciano Muñoz <luchisds@gmail.com>
2022-10-31 00:07:41 -0300

    ✨ Setup manifest.json and add app icons & Pre-cache icons and add a clear cache functionality to SW

--- cobranzas/collection/templates/sw.js
+++ cobranzas/collection/templates/sw.js
@@ -1,7 +1,18 @@
+{% load static %}
+
 const VERSION = '{{ version }}';
 const CACHE_NAME = 'collection';
 const URLS_TO_CACHE = [
   "{% url 'offline' %}",
+  "{% static 'css/styles.css' %}",
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js',
+  "{% static 'img/icon/icon-92x92.png' %}",
+  "{% static 'img/icon/icon-192x192.png' %}",
+  "{% static 'img/icon/icon-256x256.png' %}",
+  "{% static 'img/icon/icon-384x384.png' %}",
+  "{% static 'img/icon/icon-512x512.png' %}",
 ];
 
 // install files needed offline
@@ -28,5 +39,18 @@ self.addEventListener('fetch', event => {
       .catch(() => {
         return caches.match("{% url 'offline' %}");
       })
+  );
+});
+
+// Clear cache on activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cache => {
+      return Promise.all(
+        cache
+        .filter(cache => (cache.startsWith(CACHE_NAME)))
+        .map(cache => caches.delete(cache))
+      );
+    })
   );
 });
