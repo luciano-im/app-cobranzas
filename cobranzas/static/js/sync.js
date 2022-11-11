c0cd711ad8d295916369b2432d03724c51863c2e
Luciano Muñoz <luchisds@gmail.com>
2022-11-11 14:04:35 -0300

    🚩 Check for indexedDB support

--- cobranzas/static/js/sync.js
+++ cobranzas/static/js/sync.js
@@ -11,11 +11,23 @@ const URL = `/collections/data/`;
 //// DOM ACCESS ////
 
 const syncButton = document.querySelector('.synchronization button');
+const syncDate = document.querySelector('.synchronization .sync-date');
+const syncLastUpdate = document.querySelector('.synchronization .last-sync');
+const syncNotSupported = document.querySelector('.synchronization .idxdb-support');
 
 
 //// EVENTS ////
 
 syncButton.addEventListener('click', (e) => {
+  // Check for indexedDB support
+  if (!('indexedDB' in window)) {
+    console.log("This browser doesn't support IndexedDB");
+    syncNotSupported.classList.add('show');
+    return;
+  } else {
+    syncDate.classList.add('show');
+  }
+
   fetchAPI(URL, 'GET', 'application/json').then((res) => {
     if (res) {
       console.log(res);
