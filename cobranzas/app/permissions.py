32ccf5597cca9aae1254b86f64c497a2ffbdf279
Luciano Muñoz <luchisds@gmail.com>
2023-06-21 23:40:13 -0300

    🛂 Added redirect to login page

--- cobranzas/app/permissions.py
+++ cobranzas/app/permissions.py
@@ -5,8 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class AdminPermission(PermissionRequiredMixin):
     """Verify that the current user is a staff member."""
     permission_denied_message = _("You don't have permission to access this page.")
-    # TODO: Redirect to homepage
-    login_url = '/'
+    login_url = '/login/'
 
     def has_permission(self):
         return True if self.request.user.is_staff else False
