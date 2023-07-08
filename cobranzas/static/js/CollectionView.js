fb2f1781a3e1b9deac9f960c3c33af803c6c4d5b
Luciano Muñoz <luchisds@gmail.com>
2023-07-08 12:11:19 -0300

    ✨ Create CollectionView class

--- cobranzas/static/js/CollectionView.js
+++ cobranzas/static/js/CollectionView.js
@@ -7,18 +7,31 @@ export class CollectionView {
   * Creates an instance of Collection View
   *
   * @param {Collection} collection Receives an instance of Collection.
+  * @param {object} parent A DOM element where the collection will be displayed
   */
-  constructor(collection) {
+  constructor(collection, parent) {
     this.collection = collection;
+    this.total = 0;
+    this.totalTag = null;
+    this.submitButton = null;
+
+    // UNBIND EVENTS
+    // After re-rendering the after the event listeners attached to 'root-element' still exists
+    // To prevent binding the events again, clone the element (cloneNode copy everything except event listeners)
+    const rootElement = document.createElement('div');
+    rootElement.classList.add('root-element');
+    this.rootElement = rootElement.cloneNode(true);
+    // Then empty the content of the parent element to delete content from previous View renders
+    // and append 'root-element' to the parent container
+    parent.innerHTML = '';
+    parent.appendChild(this.rootElement);
   }
 
   /**
   * Renders each sale from the collection
-  * 
-  * @param {object} rootElement A DOM element where the collection will be displayed
   */
-  render(rootElement) {
-    const formsetTotalForms = document.querySelector('.create-collection input[name="collection-TOTAL_FORMS"]');
+  render() {
+    this.setFormsetTotalForms();
 
     const sales = this.collection.getSales();
     let salesContent = '';
@@ -33,10 +46,167 @@ export class CollectionView {
                       <input type="submit" id="submit-collection" value="Guardar" class="btn btn-primary" disabled>
                     </div>`;
 
-    rootElement.innerHTML = template;
+    this.rootElement.innerHTML = template;
 
-    // Sets the value of the total forms field with the number of installments
-    formsetTotalForms.setAttribute('value', this.collection.installmentsCount());
+    this.totalTag = this.rootElement.querySelector('#total');
+    this.submitButton = this.rootElement.querySelector('#submit-collection');
+
+    this.bindEvents();
+  }
+
+  /**
+  * Sets the value of the total forms field with the number of installments
+  */
+  setFormsetTotalForms() {
+    document
+      .querySelector('input[name="collection-TOTAL_FORMS"]')
+      .setAttribute('value', this.collection.installmentsCount());
+  }
+
+  /**
+  * Attach the 'change', 'click', and 'select' events to the root element
+  */
+  bindEvents() {
+    this.rootElement.addEventListener('change', e => this._handleChangeEvent(e));
+    this.rootElement.addEventListener('click', e => this._handleClickEvent(e));
+    this.rootElement.addEventListener('select', e => this._handleSelectEvent(e));
+  }
+
+  /**
+  * Handles 'change' event when event target is a checkbox or an input of type number
+  * 
+  * @param {object} event An addEventListener event object
+  */
+  _handleChangeEvent(event) {
+    if (event.target.type == 'checkbox') {
+      this._handleCheckboxChangeEvent(event.target);
+    }
+
+    if (event.target.type == 'number') {
+      this._handlePaymentInputChangeEvent(event.target);
+    }
+  }
+
+  /**
+  * Handles 'click' event when event target is an input of type number
+  * 
+  * @param {object} event An addEventListener event object
+  */
+  _handleClickEvent(event) {
+    if (event.target.type == 'number') {
+      this._handlePaymentInputClickSelectEvent(event.target);
+    }
+  }
+
+  /**
+  * Handles 'select' event when event target is an input of type number
+  * 
+  * @param {object} event An addEventListener event object
+  */
+  _handleSelectEvent(event) {
+    if (event.target.type == 'number') {
+      this._handlePaymentInputClickSelectEvent(event.target);
+    }
+  }
+
+  /**
+  * Handles actions when a checkbox is clicked
+  * 
+  * @param {object} checkbox A checkbox element
+  */
+  _handleCheckboxChangeEvent(checkbox) {
+    // Get the form ID from checkbox name attribute
+    // const formID = checkbox.name.split('-')[2];
+    const { saleID, formID } = { ...this._extractSaleFormID(checkbox.name) };
+    console.log(saleID, formID);
+    // Get amount input, installment amount and paid amount values
+    const paymentInput = this.rootElement.querySelector(`#id_collection-${saleID}-${formID}-amount`);
+    const totalAmount = parseFloat(this.rootElement.querySelector(`#id_collection-${saleID}-${formID}-installment_amount`).value);
+    const paidAmount = parseFloat(this.rootElement.querySelector(`#id_collection-${saleID}-${formID}-paid_amount`).value);
+
+    // Preserves old value in the data attribute "data-old-value"
+    paymentInput.dataset.oldValue = paymentInput.value || 0.0;
+
+    // When checkbox change its state:
+    // 1. the amount input is completed with the pending amount
+    // 2. the total is updated
+    if (checkbox.checked) {
+      const payment = paidAmount > 0 ? totalAmount - paidAmount : totalAmount;
+      paymentInput.value = payment;
+      this._updateTotal(payment);
+    } else {
+      this._updateTotal(-parseFloat(paymentInput.value));
+      paymentInput.value = 0.0;
+    }
+  };
+
+  /**
+  * Handles actions when the payment input changes
+  * 
+  * @param {object} input An input element
+  */
+  _handlePaymentInputChangeEvent(input) {
+    // Get form ID
+    // const formID = input.name.split('-')[2];
+    const { saleID, formID } = { ...this._extractSaleFormID(input.name) };
+    // Check the checkbox field
+    this.rootElement.querySelector(`input[name=collection-${saleID}-${formID}-checked]`).checked = true;
+
+    // The change event in amount inputs updates values and totals
+    const oldValue = parseFloat(input.dataset.oldValue) || 0.0;
+    const currentValue = parseFloat(input.value) || 0.0;
+    const maxValue = parseFloat(input.max) || 0.0;
+
+    // If the value entered by the user is greater than the maximum value
+    // then use maxValue as the currentValue
+    if (currentValue > maxValue) {
+      input.value = maxValue.toFixed(2);
+      this._updateTotal(maxValue - oldValue);
+    } else {
+      this._updateTotal(currentValue === 0.0 ? -oldValue : currentValue - oldValue);
+    }
+  }
+
+  /**
+  * Handles actions when the payment input content is selected
+  * 
+  * @param {object} input An input element
+  */
+  _handlePaymentInputClickSelectEvent(input) {
+    // When an amount input is clicked, the "data-old-value" attribute is updated
+    // The select event fires when some text has been selected
+    // preserves old value
+    input.dataset.oldValue = input.value;
+  }
+
+  /**
+  * Updates the total tag in the template, and manages if the submit button must be enabled
+  * or disabled, based on the calculated amount.
+  * 
+  * @param {Number} amount Receives a number to calculate the total amount
+  */
+  _updateTotal(amount) {
+    this.total += amount;
+    if (this.total == 0) {
+      this.submitButton.disabled = true;
+    } else {
+      this.submitButton.disabled = false;
+    }
+    this.totalTag.innerText = this.total.toLocaleString("es-AR", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
+  };
+
+
+  /**
+  * Extracts and returns the ID's of sale and form
+  * 
+  * @param {String} text Receives a string with format xxxx-saleID-formID-xxxx
+  */
+  _extractSaleFormID(text) {
+    const splitedText = text.split('-');
+    return { saleID: splitedText[1], formID: splitedText[2] };
   }
 }
 
@@ -151,7 +321,7 @@ class SaleInstallmentsComponent {
   createInstallmentsLayout(installments) {
     let layout = '';
     installments.map(installment => {
-      const saleInstalmentForm = new SaleInstallmentFormComponent(installment, this.formID)
+      const saleInstalmentForm = new SaleInstallmentFormComponent(installment, this.saleID, this.formID)
       layout += saleInstalmentForm.render();
       this.formID++;
     });
@@ -168,10 +338,12 @@ class SaleInstallmentFormComponent {
   * Creates an instance of the sale installment form class 
   *
   * @param {Installment} props An instance of the Installment class
+  * @param {Number} saleID The ID of the current sale
   * @param {Number} formID The ID to assign to the form
   */
-  constructor(props, formID) {
+  constructor(props, saleID, formID) {
     this.props = props;
+    this.saleID = saleID;
     this.formID = formID;
   }
 
@@ -202,7 +374,7 @@ class SaleInstallmentFormComponent {
                         </div>
                       </td>
                     </tr>`
-      .replace(/__prefix__/g, this.formID);
+      .replace(/__prefix__/g, `${this.saleID}-${this.formID}`);
 
     return template;
   }
