1c29ad657bb1c636ec8e75ff80336f3414cb3a6c
Luciano Muñoz <luchisds@gmail.com>
2024-06-02 12:08:41 -0300

    🔖 Bump app to version 1.6

--- cobranzas/collection/views.py
+++ cobranzas/collection/views.py
@@ -139,7 +139,7 @@ class ServiceWorkerView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['version'] = '1.5'
+        context['version'] = '1.6'
         return context
 
 
